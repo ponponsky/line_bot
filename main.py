54d5e18050c882547ba28bf19ae5f880279e8bbe
ponponsky <shinya187r@gmail.com>
2020-05-16 13:41:58 +0900

    QuickReply4

--- main.py
+++ main.py
@@ -10,18 +10,14 @@ from linebot.models import (MessageEvent, TextMessage, TextSendMessage, QuickRep
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
-def GetCertInfo():
-    channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-    channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-    if channel_secret is None:
-        print('Specify LINE_CHANNEL_SECRET as environment variable.')
-        sys.exit(1)
-    if channel_access_token is None:
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
         print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
         sys.exit(1)
-    return [channel_secret, channel_access_token]
-
-channel_access_token,channel_secret = GetCertInfo()
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
@@ -29,11 +25,9 @@ handler = WebhookHandler(channel_secret)
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
