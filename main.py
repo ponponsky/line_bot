61a30e8bc905a9d185b8b10303a0b29d22156ee6
ponponsky <shinya187r@gmail.com>
2020-05-16 13:13:26 +0900

    QuickReply1

--- main.py
+++ main.py
@@ -1,46 +1,29 @@
 # -*- coding: utf-8 -*-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-import repository
 import os
 import sys
 from argparse import ArgumentParser
-
 from flask import Flask, request, abort
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (MessageEvent, TextMessage, TextSendMessage, QuickReplyButton, MessageAction, QuickReply)
 
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
+channel_access_token,channel_secret = GetCertInfo()
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+def GetCertInfo():
+    channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+    channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+    if channel_secret is None:
+        print('Specify LINE_CHANNEL_SECRET as environment variable.')
+        sys.exit(1)
+    if channel_access_token is None:
+        print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+        sys.exit(1)
+    return [channel_secret, channel_access_token]
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -59,15 +42,17 @@ def callback():
 
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
-    return_message = repository.Execute_Text(event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=return_message)
+    choiceList_Text = ["選択肢①","選択肢②","選択肢③"]
+    items = [
+        QuickReplyButton(action=MessageAction(label=f"{text}", text=f"{text}_text"))
+        for text in choiceList_Text
+    ]
+    messages = TextSendMessage(
+        text="↓選択肢を選んでね↓"
+        ,quick_reply=QuickReply(items=items)
     )
-
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
